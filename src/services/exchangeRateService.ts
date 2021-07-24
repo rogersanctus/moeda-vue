3f3d300d783e663c08a855ec61400e70e8b88121
rogersanctus <rogersanctus@gmail.com>
2021-07-24 00:52:51 -0300

    Criado service para requisições à API de Taxa de Câmbio

---
+++ src/services/exchangeRateService.ts
@@ -0,0 +1,85 @@
+import {TimeSeries, SymbolsResponse} from '@t/timeSeries'
+
+const EXCHANGE_RATE_API = process.env.EXCHANGE_RATE_API
+const TIME_SERIES = 'timeseries'
+const SYMBOLS = 'symbols'
+
+function formatDate(date: Date) {
+    if (isNaN(date.valueOf())) {
+      return undefined
+    }
+  
+    return (
+      date.getFullYear() +
+      '-' +
+      (date.getMonth() + 1 + '').padStart(2, '0') +
+      '-' +
+      date.getDate()
+    )
+  }
+  
+
+function buildTimeSeriesQuery(
+    fromDate: Date,
+    toDate: Date
+  ): string | undefined {
+    if (isNaN(fromDate.valueOf()) || isNaN(toDate.valueOf())) {
+      return undefined
+    }
+  
+    return (
+      'start_date=' + formatDate(fromDate) + '&end_date=' + formatDate(toDate)
+    )
+  }
+
+  export async function fetchSymbols(): Promise<SymbolsResponse> {
+    return new Promise((resolve, reject) => {
+      if(!EXCHANGE_RATE_API || !SYMBOLS) {
+          reject(new Error('Request URL is empty or NULL for fetchSymbols'))
+      }
+  
+      fetch(EXCHANGE_RATE_API + '/' + SYMBOLS, {
+        method: 'GET',
+      })
+        .then((resp) => {
+          resp.json().then((data) => {
+            resolve(data)
+          })
+        })
+        .catch((error) => reject(error))
+    })
+  }
+
+export async function fetchSeries(
+    fromDate: Date,
+    toDate: Date,
+    base: string,
+    cmp?: string
+  ): Promise<TimeSeries> {
+    return new Promise((resolve, reject) => {
+      const timeQueryString = buildTimeSeriesQuery(fromDate, toDate)
+  
+      if (!timeQueryString) {
+        reject(
+          new Error(
+            'Invalid date intervals. ' +
+              JSON.stringify({ start_date: fromDate, end_date: toDate })
+          )
+        )
+      }
+
+      if(!EXCHANGE_RATE_API || !TIME_SERIES) {
+          reject(new Error('Request URL is empty or NULL for fetchSeries'))
+      }
+  
+      fetch(EXCHANGE_RATE_API + '/' + TIME_SERIES + '?' + timeQueryString + '&base=' + base + (cmp? '&symbols=' + cmp : ''), {
+        method: 'GET',
+      })
+        .then((resp) => {
+          resp.json().then((data) => {
+            resolve(data)
+          })
+        })
+        .catch((error) => reject(error))
+    })
+  }
