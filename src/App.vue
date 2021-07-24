4188ad7be4b41b7833b82e3defa833c530d9e5cc
rogersanctus <rogersanctus@gmail.com>
2021-07-23 21:29:03 -0300

    Primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <Dashboard />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Dashboard from '@/components/Dashboard.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Dashboard
+  }
+})
+</script>
+
+<style>
+body {
+  margin: 0;
+  overflow: hidden;
+}
+
+#app {
+  height: 100vh;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+
+</style>
---
+++ src/components/Dashboard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="view">
+    <div style="display: flex">
+      <span>Cambio dos últimos 30 dias:</span>
+      <button style="margin-left: auto" @click="updateSeries">Update</button>
+    </div>
+    <!-- Chart -->
+    <footer class="footer">RODAPÉ</footer>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Dashboard',
+
+  setup() {
+    return {
+    }
+  },
+})
+</script>
+<style lang="scss" scoped>
+.view {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+
+  footer {
+    margin-top: auto;
+  }
+}
+</style>
