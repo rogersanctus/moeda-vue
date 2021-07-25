2495be528fc56f5308374c9c6236a5139ae34464
rogersanctus <rogersanctus@gmail.com>
2021-07-24 22:38:24 -0300

    Adaptação do início do nome da variável de ambiente para o Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-//const path = require('path')
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
@@ -8,11 +7,16 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@t': path.resolve(__dirname, './types')
+      '@': path.resolve(process.cwd(), './src'),
+      '@t': path.resolve(process.cwd(), './types')
     }
   },
   build: {
     sourcemap: true
+  },
+  server: {
+    fs: {
+      allow: ['./src', './types', './node_modules']
+    }
   }
 })
