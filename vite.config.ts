7385b3c9c0a03fd2cb9814311a3a7812a30f4640
rogersanctus <rogersanctus@gmail.com>
2021-07-30 23:57:00 -0300

    * Ajustes no Vite para usar mapeamento de aliases em URL;

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(process.cwd(), './src'),
-      '@t': path.resolve(process.cwd(), './types')
+      '@': new URL('./src/', import.meta.url).pathname,
+      '@t': new URL('./types/', import.meta.url).pathname
     }
   },
   build: {
